a348118bcca3b640fedc72bf19c7d90c800a4cda
UtsavAgrawal23 <utsav2389@gmail.com>
2020-12-21 15:53:06 +0530

    1 file modified adding hostLisitiner

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,7 +1,20 @@
-import {Directive} from "@angular/core"
+import {Directive, HostListener} from "@angular/core"
 @Directive({
   selector:'[credit-card]'
 })
 export class CreditCard{
+  @HostListener('input',['$event'])
+    OnKeyDown(e){
+      const input  = e.target;
+      let trimmed = input.value.replace(/\s+/g,'');
+      if(trimmed.length>16) {
+        trimmed = trimmed.substr(0,16);
+      }
+      let numbers = [];
+      for(let i = 0; i < trimmed.length; i += 4){
+        numbers.push(trimmed.substr(i,4));
+      }
+        input.value = numbers.join(' ');
+    }
 
 }
