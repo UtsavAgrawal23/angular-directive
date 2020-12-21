33df6eeb05f99e71d98f794685cbe424331d1f32
UtsavAgrawal23 <utsav2389@gmail.com>
2020-12-21 15:23:14 +0530

    Initial setup for directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CreditCard } from './credit-card/credit-card.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, CreditCard ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/credit-card/credit-card.directive.ts
@@ -0,0 +1,7 @@
+import {Directive} from "@angular/core"
+@Directive({
+  selector:'[credit-card]'
+})
+export class CreditCard{
+
+}
